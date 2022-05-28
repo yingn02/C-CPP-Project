a97d2fab7339187c9241bcb5386aed250f622772
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-05-28 15:59:16 +0900

    Add files via upload

--- Ting-튕겨내기게임/pro.c
+++ Ting-튕겨내기게임/pro.c
@@ -44,6 +44,7 @@
 #define RIGHT2	'd'
 
 #define J	'j'
+#define SPACE ' '
 
 #define WIDTH 140
 #define HEIGHT 70
@@ -56,6 +57,8 @@ int keep_moving = 1; // 1:계속이동, 0:한칸씩이동.
 int time_out = 30; // 제한시간
 int score[2] = { 0 };
 int called[2];
+char team1[20] = "레드팀";
+char team2[20] = "블루팀";
 
 void removeCursor(void) { // 커서를 안보이게 한다
 
@@ -85,6 +88,330 @@ void cls(int bg_color, int text_color)
 	system(cmd);
 }
 
+void intro(void) {
+	unsigned char ch;
+	int x, y;
+	int len = strlen("■");
+
+	textcolor(BLACK, WHITE);
+	for (x = 0; x <= 138; x += len) {
+		gotoxy(x, 0);
+		printf("%s", "■");
+		gotoxy(x, 68);
+		printf("%s", "■");
+	}
+	for (y = 0; y <= 68; y++) {
+		gotoxy(0, y);
+		printf("%s", "■");
+		gotoxy(138, y);
+		printf("%s", "■");
+	}
+
+	//인트로 "탁구"
+	textcolor(RED1, WHITE);
+	gotoxy(10, 5);
+	printf("■■■■   ■         ■■■■■■");
+	gotoxy(10, 6);
+	printf("■         ■                   ■");
+	gotoxy(10, 7);
+	printf("■■■■   ■■                 ■");
+	gotoxy(10, 8);
+	printf("■         ■                   ■");
+	gotoxy(10, 9);
+	printf("■■■■   ■                        ");
+	gotoxy(10, 10);
+	printf("                     ■■■■■■■ ");
+	gotoxy(10, 11);
+	printf("    ■■■■               ■");
+	gotoxy(10, 12);
+	printf("          ■               ■");
+	gotoxy(10, 13);
+	printf("          ■               ■");
+	gotoxy(10, 14);
+	printf("            ");
+	gotoxy(10, 15);
+	printf("            ");
+	textcolor(BLUE1, WHITE);
+	gotoxy(10, 17);
+	printf("■■■■  ■ ■        ■■     ■");
+	gotoxy(10, 18);
+	printf("      ■  ■ ■      ■    ■   ■");
+	gotoxy(10, 19);
+	printf("      ■■■ ■      ■    ■   ■");
+	gotoxy(10, 20);
+	printf("      ■  ■ ■        ■■     ■");
+	gotoxy(10, 21);
+	printf("      ■  ■ ■           ");
+	gotoxy(10, 22);
+	printf("      ■  ■ ■          ■■■■");
+	gotoxy(10, 23);
+	printf("          ■ ■          ■    ■");
+	gotoxy(10, 24);
+	printf("                         ■■■■");
+
+	textcolor(RED1, WHITE);
+
+	//인트로 BLUE
+	textcolor(BLUE2, WHITE);
+	gotoxy(70, 10);
+	printf("                                  ■■■■");
+	textcolor(BLUE1, WHITE);
+	gotoxy(70, 11);
+	printf("               ■■■■■        ■■■■■");
+	gotoxy(70, 12);
+	printf("               ■■■■■       ■■■■■■");
+	textcolor(BLUE2, WHITE);
+	gotoxy(70, 13);
+	printf("               ■■■■■       ■■■■■■");
+	gotoxy(70, 14);
+	printf("               ■■■■■       ■■■■■■");
+	gotoxy(70, 15);
+	printf("               ■■■■■        ■■■■■");
+	gotoxy(70, 16);
+	printf("                  ■■            ■■■■   ");
+	gotoxy(70, 17);
+	printf("                  ■■        ■■■■■■■■");
+	gotoxy(70, 18);
+	printf("                  ■■■  ■■■■■■■■■■■■");
+	gotoxy(70, 19);
+	printf("                     ■■■■■  ■■■■■■■■■■");
+	gotoxy(70, 20);
+	printf("                       ■■■     ■■■■■■   ■■■");
+	gotoxy(70, 21);
+	printf("                                  ■■■■■■    ■■■");
+	gotoxy(70, 22);
+	printf("                                  ■■■■■■    ■■■");
+	//인트로 탁구대
+	textcolor(BLUE1, WHITE);
+	gotoxy(70, 23);
+	printf("        □□□□□□□□□□□□□□□□□□□□□□□□□□");
+	gotoxy(69, 24);
+	printf("        □■■■■■■■■■■■□■■■■■■■■■■■■□");
+	gotoxy(66, 25);
+	printf("        □■■■■■■■■■■■□■■■■■■■■■■■■■□");
+	gotoxy(65, 26);
+	printf("        □■■■■■■■■■■■□■■■■■■■■■■■■■□");
+	gotoxy(62, 27);
+	printf("        □■■■■■■■■■■■□■■■■■■■■■■■■■■□");
+	gotoxy(61, 28);
+	printf("        □■■■■■■■■■■■□■■■■■■■■■■■■■■□");
+	gotoxy(58, 29);
+	printf("        □■■■■■■■■■■■□■■■■■■■■■■■■■■■□");
+	gotoxy(57, 30);
+	printf("        □■■■■■■■■■■■□■■■■■■■■■■■■■■■□");
+	gotoxy(53, 31);
+	printf("        □■■■■■■■■■■■□■■■■■■■■■■■■■■■■■□");
+	gotoxy(52, 32);
+	printf("        □■■■■■■■■■■■□■■■■■■■■■■■■■■■■■□");
+	gotoxy(47, 33);
+	printf("        □□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□");
+	gotoxy(46, 34);
+	printf("        □■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■□");
+	gotoxy(42, 35);
+	printf("        □■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■□");
+	gotoxy(41, 36);
+	printf("        □■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■□");
+	gotoxy(37, 37);
+	printf("        □■■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■□");
+	gotoxy(36, 38);
+	printf("        □■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(31, 39);
+	printf("        □■■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(30, 40);
+	printf("        □■■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(25, 41);
+	printf("        □■■■■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(24, 42);
+	printf("        □■■■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(20, 43);
+	printf("        □■■■■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(19, 44);
+	printf("        □■■■■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(14, 45);
+	printf("        □■■■■■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(13, 46);
+	printf("        □■■■■■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(8, 47);
+	printf("        □■■■■■■■■■■■■■■■■■■■□■■■■■■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(7, 48);
+	printf("        □■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(3, 49);
+	printf("        □■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■□");
+	gotoxy(2, 50);
+	printf("        □□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□");
+
+
+
+	//인트로 RED
+	textcolor(RED2, WHITE);
+	gotoxy(40, 38);
+	printf("■■■■■■");
+	gotoxy(39, 39);
+	printf("■■■■■■■");
+	textcolor(RED1, WHITE);
+	gotoxy(38, 40);
+	printf("■■■■■■■■");
+	gotoxy(37, 41); 
+	printf("■■■■■■■■■");
+	gotoxy(37, 42);
+	printf("■■■■■■■■■");
+	textcolor(RED2, WHITE);
+	gotoxy(37, 43);
+	printf("■■■■■■■■■");
+	gotoxy(38, 44);
+	printf("■■■■■■■■");
+	gotoxy(39, 45);
+	printf("■■■■■■■");
+	gotoxy(40, 46);
+	printf("■■■■■■");
+	gotoxy(39, 47);
+	printf("■■■■■■■");
+	gotoxy(30, 48);
+	printf("■■■■■■■■■■■■■■■■■");
+	gotoxy(24, 49);
+	printf("■■■■■■■■■■■■■■■■■■■■");
+	gotoxy(24, 50);
+	printf("■■■■■■■■■■■■■■■■■■■■■");
+	gotoxy(20, 51);
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■   ■■■■");
+	gotoxy(20, 52);
+	printf("■■■■■■■■■■■■■■■■■■    ■■■■■■■■■");
+	gotoxy(20, 53);
+	printf("■■■■  ■■■■■■■■■■■■■    ■■■■■■■■ ");
+	gotoxy(18, 54);
+	printf("■■■■■  ■■■■■■■■■■■■      ■■■■■■");
+	gotoxy(16, 55);
+	printf("■■■■■   ■■■■■■■■■■■■         ■■■■ ");
+	gotoxy(15, 56);
+	printf("  ■■■■    ■■■■■■■■■■■■      ");
+	gotoxy(15, 57);
+	printf("  ■■■■    ■■■■■■■■■■■■      ");
+	gotoxy(15, 57);
+	printf("  ■■■■    ■■■■■■■■■■■■      ");
+	gotoxy(15, 58);
+	printf("  ■■■■    ■■■■■■■■■■■■     ");
+	gotoxy(15, 59);
+	printf("  ■■■■   ■■■■■■■■■■■■      ");
+	gotoxy(15, 60);
+	printf("  ■■■■   ■■■■■■■■■■■■     ");
+	gotoxy(15, 61);
+	printf("  ■■■■■  ■■■■■■■■■■■■      ");
+	gotoxy(15, 62);
+	printf("  ■■■■■  ■■■■■■■■■■■■     ");
+	gotoxy(15, 63);
+	printf("  ■■■■    ■■■■■■■■■■■■     ");
+
+	//인트로 탁구채
+	textcolor(BLUE1, WHITE);
+	gotoxy(85, 11);
+	printf("■■■■■");
+	gotoxy(85, 12);
+	printf("■■■■■");
+	gotoxy(85, 13);
+	printf("■■■■■");
+	gotoxy(85, 14);
+	printf("■■■■■");
+	textcolor(YELLOW1, WHITE);
+	gotoxy(85, 15);
+	printf("■■■■■");
+	gotoxy(88, 16);
+	printf("■■");
+	gotoxy(88, 17);
+	printf("■■");
+
+	textcolor(RED1, WHITE);
+	gotoxy(73, 44);
+	printf("■■■■■");
+	gotoxy(73, 45);
+	printf("■■■■■");
+	gotoxy(73, 46);
+	printf("■■■■■");
+	gotoxy(73, 47);
+	printf("■■■■■");
+	textcolor(YELLOW1, WHITE);
+	gotoxy(73, 48);
+	printf("■■■■■");
+	gotoxy(76, 49);
+	printf("■■");
+	gotoxy(76, 50);
+	printf("■■");
+
+	//인트로 공
+	textcolor(YELLOW1, WHITE);
+	gotoxy(70, 20);
+	printf("■■");
+	gotoxy(70, 21);
+	printf("■■");
+
+	//인트로 네트
+	textcolor(GRAY2, WHITE);
+	gotoxy(56, 30);
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+	gotoxy(56, 31);
+	printf("■");
+	gotoxy(56, 32);
+	printf("■");
+	gotoxy(122, 31);
+	printf("■");
+	gotoxy(122, 32);
+	printf("■");
+
+
+	while (1) { // 스페이스 바를 누르면 게임시작
+		if (_kbhit()) {
+			ch = _getch();
+			if (ch == SPACE) {
+				for (int i = 0; i < 5; i++) {
+					textcolor(BLACK, WHITE);
+					gotoxy(52, 64);
+					printf("                                     ");
+					gotoxy(52, 65);
+					printf("  시작하려면 스페이스 바를 누르세요  ");
+					gotoxy(52, 66);
+					printf("                                     ");
+					Sleep(70);
+					textcolor(WHITE, BLACK);
+					gotoxy(52, 64);
+					printf("                                     ");
+					gotoxy(52, 65);
+					printf("  시작하려면 스페이스 바를 누르세요  ");
+					gotoxy(52, 66);
+					printf("                                     ");
+					Sleep(70);
+				}
+				textcolor(WHITE, BLACK);
+				gotoxy(52, 64);
+				printf("                                     ");
+				gotoxy(52, 65);
+				printf("  시작하려면 스페이스 바를 누르세요  ");
+				gotoxy(52, 66);
+				printf("                                     ");
+				Sleep(1000);
+				break;
+			}
+				
+		}
+		else {
+			textcolor(BLACK, WHITE);
+			gotoxy(52, 64);
+			printf("                                     ");
+			gotoxy(52, 65);
+			printf("  시작하려면 스페이스 바를 누르세요  ");
+			gotoxy(52, 66);
+			printf("                                     ");
+			Sleep(500);
+			textcolor(WHITE, BLACK);
+			gotoxy(52, 64);
+			printf("                                     ");
+			gotoxy(52, 65);
+			printf("  시작하려면 스페이스 바를 누르세요  ");
+			gotoxy(52, 66);
+			printf("                                     ");
+			Sleep(500);
+		}
+	}
+}
+
 void putplayer1(int x, int y) {
 	gotoxy(x, y); 
 	textcolor(RED1, WHITE);
@@ -95,12 +422,13 @@ void putplayer1(int x, int y) {
 	printf("■■■■■\n");
 	gotoxy(x, y+3);
 	printf("■■■■■\n");
+	textcolor(YELLOW1, WHITE);
 	gotoxy(x, y+4);
-	printf("□□□□□\n");
+	printf("■■■■■\n");
 	gotoxy(x, y+5);
-	printf("   □□\n");
+	printf("   ■■\n");
 	gotoxy(x, y+6);
-	printf("   □□\n");
+	printf("   ■■\n");
 	textcolor(WHITE, WHITE);
 }
 
@@ -114,12 +442,13 @@ void putplayer2(int x, int y) {
 	printf("■■■■■\n");
 	gotoxy(x, y + 3);
 	printf("■■■■■\n");
+	textcolor(YELLOW1, WHITE);
 	gotoxy(x, y + 4);
-	printf("□□□□□\n");
+	printf("■■■■■\n");
 	gotoxy(x, y + 5);
-	printf("   □□\n");
+	printf("   ■■\n");
 	gotoxy(x, y + 6);
-	printf("   □□\n");
+	printf("   ■■\n");
 	textcolor(WHITE, WHITE);
 }
 
@@ -166,8 +495,11 @@ void puttable(int x, int y) {
 	textcolor(WHITE, WHITE);
 }
 
-void putball(int x, int y) {
-	textcolor(YELLOW1, WHITE);
+void putball(int x, int y, int speed) {
+	if(speed == 0)
+		textcolor(RED2, WHITE);
+	else
+		textcolor(YELLOW1, WHITE);
 	gotoxy(x, y);
 	printf("■■");
 	gotoxy(x, y+1);
@@ -610,24 +942,17 @@ void player2(unsigned char ch, int *p2x, int *p2y) {
 	}
 }
 
-// box 그리기 함수, ch 문자열로 (x1,y1) ~ (x2,y2) box를 그린다.
-// 한글 문자를 그리는 용도로 사용 "□" 로 벽을 그리는 경우
-void draw_box(int x1, int y1, int x2, int y2, char* ch)
-{
-	int x, y;
-	int len = strlen(ch);
-	for (x = x1; x <= x2; x += len) {
-		gotoxy(x, y1);
-		printf("%s", ch);
-		gotoxy(x, y2);
-		printf("%s", ch);
-	}
-	for (y = y1; y <= y2; y++) {
-		gotoxy(x1, y);
-		printf("%s", ch);
-		gotoxy(x2, y);
-		printf("%s", ch);
-	}
+void teamname(void) {
+	textcolor(RED1, WHITE);
+	gotoxy(58, 20);
+	printf("레드팀의 이름을 적어주세요");
+	gotoxy(58, 22);
+	scanf("%s", team1);
+	textcolor(BLUE1, WHITE);
+	gotoxy(58, 30);
+	printf("블루팀의 이름을 적어주세요");
+	gotoxy(58, 32);
+	scanf("%s", team2);
 }
 
 //게임 초기화, 처음 시작과 Restar때 호출
@@ -653,20 +978,30 @@ void init_game() {
 
 void main() {//////////////////////////////////////////////////////////////////////main
 	unsigned char ch;
-	char team1[20] = "레드팀";
-	char team2[20] = "블루팀";
 	int p1x, p1y, p2x, p2y;
 	int sv, nball_x, nball_y, oball_x, oball_y;
-	int goal_x, goal_y, ballmove;
+	int goal_x, goal_y, ballmove, spike, speed;
 	int win, score1, score2;
 
 	//인트로
+	intro();
 
+	// 화면 비우기
+	textcolor(WHITE, WHITE);
+	for (int i = 0; i < WIDTH; i++) {
+		for (int j = 0; j < HEIGHT; j++) {
+			gotoxy(i, j);
+			printf(" ");
+		}
+	}
+
+	//팀명을 적으시오
+	teamname();
 
 START: //경기를 처음부터 다시 할 경우
 	p1x = 60, p1y = 60, p2x = 15, p2y = 3;
 	sv = 0, nball_x = 0, nball_y = 0, oball_x = 0, oball_y = 0;
-	goal_x = 0, goal_y = 0, ballmove = 1;
+	goal_x = 0, goal_y = 0, ballmove = 1, spike = 1, speed = 5;
 	win = 0, score1 = 0, score2 = 0;
 
 	init_game();
@@ -691,7 +1026,8 @@ START: //경기를 처음부터 다시 할 경우
 	srand(time(NULL));	
 	sv = 1 + rand() % 2; //랜덤 서브, 1이면 player1 2면 player2
 
-SET: //경기 셋팅, 점수가 났을 경우 또 불러온다
+SET: //경기 세팅, 점수가 났을 경우 또 불러온다
+	spike = 1;
 	if (win == 1) sv == 1;
 	else if (win == 2) sv == 2;
 
@@ -705,8 +1041,7 @@ SET: //경기 셋팅, 점수가 났을 경우 또 불러온다
 		oball_x = p2x + 16, oball_y = p2y + 3;
 		goal_x = 0, goal_y = 68;
 	}
-
-	putball(oball_x, oball_y); //공 배치
+	putball(oball_x, oball_y, speed); //공 배치
 
 	goal_x = 5 + rand() % TABLEWIDTH - 5; //랜덤으로 공이 향하는 좌표의 x값 정함
 
@@ -729,6 +1064,7 @@ SET: //경기 셋팅, 점수가 났을 경우 또 불러온다
 		//탁구대
 		puttable(5, 12);
 		//공
+
 		if (sv == 1) {
 			goal_y = 0;
 			if (oball_y > goal_y) { //y축
@@ -760,6 +1096,7 @@ SET: //경기 셋팅, 점수가 났을 경우 또 불러온다
 			}
 			else if (p2y <= oball_y && oball_y <= p2y + 7) {
 				if (p2x <= oball_x && oball_x <= p2x + 7) {// 받아치면
+					spike = rand() % 11;
 				    goal_x = 5 + rand() % TABLEWIDTH - 5; //랜덤으로 공이 향하는 좌표의 x값 정함
 					goal_y = 0;
 					sv = 2;
@@ -797,23 +1134,23 @@ SET: //경기 셋팅, 점수가 났을 경우 또 불러온다
 			}
 			else if (p1y <= oball_y && oball_y <= p1y + 7) {
 				if (p1x <= oball_x && oball_x <= p1x + 7) {// 받아치면
+					spike = rand() % 11;
 				    goal_x = 5 + rand() % TABLEWIDTH - 5; //랜덤으로 공이 향하는 좌표의 x값 정함
 					goal_y = 0;
 					sv = 1;
 				}
 			}
 		}
-
+		if (spike == 0) speed = 0;
+		else speed = 5;
 		if (ballmove) {
 			eraseball(oball_x, oball_y); // 마지막 위치의 # 를 지우고
-			putball(nball_x, nball_y); // 새로운 위치에서 # 를 표시한다.
+			putball(nball_x, nball_y, speed); // 새로운 위치에서 # 를 표시한다.
 			oball_x = nball_x; // 마지막 위치를 기억한다.
 			oball_y = nball_y;
 			ballmove = 1; // 1:계속 이동, 0:한칸씩 이동
-			Sleep(10);
+			Sleep(speed); // 10%의 확률로 스파이크, 플레이어와 공의 속도가 빨라짐
 		}
-
-
 
 
 		if (_kbhit() == 1) {  // 키보드가 눌려져 있으면
@@ -870,23 +1207,23 @@ SET: //경기 셋팅, 점수가 났을 경우 또 불러온다
 		gotoxy(30, 9);
 		printf("                                                                               ");
 		gotoxy(30, 10);
-		printf("       ■■■■    ■   ■■■■    ■     ■■■■■    ■■■■   ■         ");
+		printf("       ■■■■    ■   ■■■■    ■     ■■■■■     ■■■■■■         ");
 		gotoxy(30, 11);
-		printf("             ■  ■■         ■    ■         ■        ■         ■         ");
+		printf("             ■  ■■         ■    ■         ■                   ■         ");
 		gotoxy(30, 12);
-		printf("             ■    ■         ■    ■      ■    ■     ■         ■■■     ");
+		printf("             ■    ■         ■    ■      ■    ■      ■■■■■■         ");
 		gotoxy(30, 13);
-		printf("             ■  ■■         ■    ■    ■         ■  ■         ■         ");
+		printf("             ■  ■■         ■    ■    ■   ■   ■    ■                   ");
 		gotoxy(30, 14);
-		printf("                   ■         ■    ■   ■■■■■■■  ■■■■   ■         ");
+		printf("                   ■         ■    ■   ■■■■■■■   ■■■■■■         ");
 		gotoxy(30, 15);
-		printf("              ■■■          ■    ■         ■           ■                 ");
+		printf("              ■■■          ■    ■                       ■   ■           ");
 		gotoxy(30, 16);
-		printf("             ■    ■               ■       ■■■         ■                 ");
+		printf("             ■    ■               ■       ■■■          ■   ■           ");
 		gotoxy(30, 17);
-		printf("             ■    ■                      ■      ■       ■                 ");
+		printf("             ■    ■                      ■      ■        ■   ■           ");
 		gotoxy(30, 18);
-		printf("              ■■■                         ■■■         ■■■■■         ");
+		printf("              ■■■                         ■■■      ■■■■■■■        ");
 		gotoxy(30, 19);
 		printf("                                                                               ");
 		
